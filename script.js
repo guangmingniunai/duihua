ebb00522ef4ba5fd1f62a536cd6b3f184292d4ff
guangmingniunai <56286369+guangmingniunai@users.noreply.github.com>
2020-04-16 09:28:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(".talk").click(function() {
+ 	let speech=$(".speech").val();
+  	$(".speech-bubble").text(speech);
+});
+$(".talk1").click(function() {
+ 	let speech=$(".speech1").val();
+  	$(".speech-bubble1").text(speech1);
+});
